4ea914a65fc52108f9b82e6ac901413145143530
treerock <treerock@myopera.com>
2014-05-09 14:13:18 +0000

    Enabled html5mode to remove # from urls

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,20 @@
 'use strict';
-
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'ngCookies',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-])
-// disabling routing as it isn't playing nicely
-// therefore partials are disabled
-// and I need to define the controller in the template.
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/play/:comp/:group', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/edit/:comp?', {templateUrl: 'partials/partial4.html', controller: 'MyCtrl4'});
-  $routeProvider.otherwise({redirectTo: '/edit/'});
-}])
-;
+angular.module('myApp', ['ngRoute', 'ngCookies', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.when('/play/:comp/:group', {
+            templateUrl: 'partials/partial1.html',
+            controller: 'MyCtrl1'
+        });
+        $routeProvider.when('/edit/:comp?', {
+            templateUrl: 'partials/partial4.html',
+            controller: 'MyCtrl4'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/edit/'
+        });
+        // use the HTML5 History API (for clean urls)
+        $locationProvider.html5Mode(true);
+    }
+]);
